368a7bd907b7f2ed1b66e3ac5df02adc6e04f029
DevDerlington <79136188+DevDerlington@users.noreply.github.com>
2024-02-20 23:36:00 -0800

    Add interface for school project

---
+++ pro.js
@@ -0,0 +1,12 @@
+const signinBtnLink = document.querySelector('.signinBtn-link');
+const signupBtnLink =  document.querySelector('.signupBtn-link');
+const wrapper = document.querySelector('.wrapper');
+
+signupBtnLink.addEventListener('click', () => {
+wrapper.classList.toggle('active');
+
+});
+
+// alert('the king is here@');
+
+
